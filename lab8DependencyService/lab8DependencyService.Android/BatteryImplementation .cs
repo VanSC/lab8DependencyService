2a49e8ad524dce79aefa71317b29370d14b7b87c
Alumno <Alumno@LAB1509-18>
2023-11-03 16:42:01 -0500

    Agregar archivos de proyecto.

---
+++ lab8DependencyService/lab8DependencyService.Android/BatteryImplementation .cs
@@ -0,0 +1,131 @@
+﻿using Android.App;
+using Android.OS;
+using lab8DependencyService.Droid;
+using Android.Content;
+using System;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BatteryImplementation))]
+namespace lab8DependencyService.Droid
+{
+    public class BatteryImplementation : IBattery
+    {
+        public BatteryImplementation()
+        {
+        }
+
+        public int RemainingChargePercent
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            var level = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+                            var scale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+
+                            return (int)Math.Floor(level * 100D / scale);
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public lab8DependencyService.BatteryStatus Status
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+                            if (isCharging)
+                                return lab8DependencyService.BatteryStatus.Charging;
+
+                            switch (status)
+                            {
+                                case (int)BatteryStatus.Charging:
+                                    return lab8DependencyService.BatteryStatus.Charging;
+                                case (int)BatteryStatus.Discharging:
+                                    return lab8DependencyService.BatteryStatus.Discharging;
+                                case (int)BatteryStatus.Full:
+                                    return lab8DependencyService.BatteryStatus.Full;
+                                case (int)BatteryStatus.NotCharging:
+                                    return lab8DependencyService.BatteryStatus.NotCharging;
+                                default:
+                                    return lab8DependencyService.BatteryStatus.Unknown;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public PowerSource PowerSource
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+
+                            if (!isCharging)
+                                return lab8DependencyService.PowerSource.Battery;
+                            else if (usbCharge)
+                                return lab8DependencyService.PowerSource.Usb;
+                            else if (acCharge)
+                                return lab8DependencyService.PowerSource.Ac;
+                            else if (wirelessCharge)
+                                return lab8DependencyService.PowerSource.Wireless;
+                            else
+                                return lab8DependencyService.PowerSource.Other;
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+    }
+}
